ec98e4d027c75883a42b3c4b06eee3a9163ed3d9
marcos13465 <mj185901@gmail.com>
2024-08-07 22:05:47 -0300

    Update wif.js

--- wif.js
+++ wif.js
@@ -1,9 +1,9 @@
 const bitcoin = require('bitcoinjs-lib');
 const bs58check = require('bs58check');
 
-// Função para converter chave hexadecimal para WIF
+// FunÃ§Ã£o para converter chave hexadecimal para WIF
 function hexToWIF(hexKey) {
-  // Adiciona o prefixo 0x80 (128 em decimal) no início da chave hexadecimal
+  // Adiciona o prefixo 0x80 (128 em decimal) no inÃ­cio da chave hexadecimal
   const prefix = Buffer.from('80', 'hex');
   const keyBuffer = Buffer.concat([prefix, Buffer.from(hexKey, 'hex')]);
 
@@ -17,7 +17,7 @@ function hexToWIF(hexKey) {
 }
 
 // Exemplo de uso
-const hexKey = '000000000000000000000000000000000000000000000000f7051f27b09112d4'; // Substitua pela sua chave hexadecimal
+const hexKey = '190263892b414d1ee35a7b147227e62db1d1c177ce4c73effe0e576054475750'; // Substitua pela sua chave hexadecimal
 const wifKey = hexToWIF(hexKey);
 
 console.log('Chave WIF:', wifKey);
